33797f599e0d49c3ca783ca7ccea06c917d79a80
comic <bitotajima@hotmail.com>
2022-04-20 16:36:11 -0300

    ✅ - Novo artigo

--- articles/ArticlesControllers.js
+++ articles/ArticlesControllers.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const slugify = require('slugify');
+const Category = require('../categories/Category');
+const Article = require('./Article');
+ 
 const router = express.Router();
 
+router.get('/admin/articles', (req, res) => {
+  res.render('admin/articles');  
+})
+
 router.get('/admin/articles/new', (req, res) => {
-  res.render('admin/articles/new');
+  Category.findAll({ raw: true }).then(categories => {
+    res.render('admin/articles/new', {categories});
+  }).catch(error => {
+    console.log(`❌ Erro ao resgatar categorias: ${error}`);
+  })
+})
+
+router.post('/admin/articles/new', (req, res) => {
+  const { title, category, body } = req.body;
+
+  console.log(body);
+
+  Article.create({
+    title,
+    body,
+    slug: slugify('title'),
+    categoryID: category,
+  }).then(() => {
+    res.redirect('/articles');
+  }).catch(error => {
+    console.log(`❌ Erro ao salvar artigo: ${error}`);
+  })
 })
 
 module.exports = router;
--- database/connection.js
+++ database/connection.js
@@ -7,7 +7,8 @@ const connection = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00' // Por padrão utiliza UTC
   }
 );
 
